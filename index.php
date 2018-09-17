d5d24f890eb8d3d1a95b2d90033d570318f65927
DESKTOP-0K884LG\Akadream <lortet.guillaume@gmail.com>
2018-09-17 13:28:20 +0200

    Version 1.0

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Bright - CSS Framework</title>
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/bright.css">
+    <link rel="stylesheet" href="css/master.css">
+  </head>
+
+  <body>
+    <?php require "mainmenu.php"; ?>
+
+    <section class="header">
+      <h1 class="header-title">Bright</h1>
+
+      <div class="header-content">
+        <p class="header-description">
+          Build modern and flexible project with our framework.
+        </p>
+
+        <div class="header-nav">
+          <a class="button" href="#">Get started</a>
+          <a class="button" href="#">Download</a>
+        </div>
+      </div>
+    </section>
+
+    <section class="section">
+      <h3 class="section-title">Setup</h3>
+
+      <div class="section-content">
+        <p class="section-description">Installation include the minimum requirement to use Bright framework.<br>
+        You can check out our Github repository to get the full source code including SCSS files, Documentation and README.</p>
+
+        <div class="code"><span class="noselect" style="color: #79b6f2; margin-right: 15px;">$</span>npm <span style="color: #fc929e;">install</span> bright</div>
+      </div>
+    </section>
+
+    <footer></footer>
+  </body>
+</html>
---
+++ mainmenu.php
@@ -0,0 +1,9 @@
+<nav class="navbar">
+  <a href="" class="navbar-title">Bright</a>
+
+  <ul class="nav">
+    <li class="nav-item"><a class="nav-link" href="">Home</a></li>
+    <li class="nav-item"><a class="nav-link" href="">Documentation</a></li>
+    <li class="nav-item"><a class="nav-link" href="">Contact</a></li>
+  </ul>
+</nav>
